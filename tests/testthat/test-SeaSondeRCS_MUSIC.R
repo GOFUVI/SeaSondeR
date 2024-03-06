84b3affbf245b7bd87fee7ae858ce9f0067e5ba8
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-03-06 10:12:41 +0100

    SMA-6 (#24)
    Eigenvalue decomposition of MUSIC matrix
    
    Implemented:
    
    - seasonder_MUSICCovDecomposition

--- tests/testthat/test-SeaSondeRCS_MUSIC.R
+++ tests/testthat/test-SeaSondeRCS_MUSIC.R
@@ -3,6 +3,7 @@
 test_that("SeaSondeRCS Related functions are defined",{
 
   expect_true(is.function(seasonder_getMUSICCov))
+  expect_true(is.function(seasonder_MUSICCovDecomposition))
 
 
 
@@ -20,11 +21,26 @@ describe("MUSIC", {
 
       test <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
 
-      expect_snapshot_value(test,style = "deparse")
+      expect_snapshot_value(test,style = "serialize")
 
     })
 
   })
 
+  describe("seasonder_MUSICCovDecomposition",{
+
+    it("should return the cov matrix",{
+
+      seasonder_cs_obj <- seasonder_createSeaSondeRCS(here::here("tests/testthat/data/CSS_V6.cs"), system.file("specs","CS_V1.yaml",package = "SeaSondeR"))
+
+      C <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
+
+      test <- seasonder_MUSICCovDecomposition(C)
+
+      expect_snapshot_value(test,style = "serialize")
+
+    })
+
+  })
 
 })
