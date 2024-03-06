846f1120fa92e7d6e37b5f4f1b4a750fae6b115d
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-03-06 18:54:37 +0100

    SMA-7 (#25)
    Compute euclidean distance to signal subspaces
    
    Implemented:
    
    - seasonder_MUSICEuclideanDistance

--- tests/testthat/test-SeaSondeRCS_MUSIC.R
+++ tests/testthat/test-SeaSondeRCS_MUSIC.R
@@ -4,6 +4,7 @@ test_that("SeaSondeRCS Related functions are defined",{
 
   expect_true(is.function(seasonder_getMUSICCov))
   expect_true(is.function(seasonder_MUSICCovDecomposition))
+  expect_true(is.function(seasonder_MUSICEuclideanDistance))
 
 
 
@@ -29,13 +30,34 @@ describe("MUSIC", {
 
   describe("seasonder_MUSICCovDecomposition",{
 
-    it("should return the cov matrix",{
+    it("should return the eigen decomposition of the cov matrix",{
 
       seasonder_cs_obj <- seasonder_createSeaSondeRCS(here::here("tests/testthat/data/CSS_V6.cs"), system.file("specs","CS_V1.yaml",package = "SeaSondeR"))
 
       C <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
 
       test <- seasonder_MUSICCovDecomposition(C)
+
+      expect_snapshot_value(test,style = "serialize")
+
+    })
+
+  })
+
+
+  describe("seasonder_MUSICEuclideanDistance",{
+
+    it("should return the euclidean distances",{
+
+      seasonder_cs_obj <- seasonder_createSeaSondeRCS(here::here("tests/testthat/data/CSS_V6.cs"), system.file("specs","CS_V1.yaml",package = "SeaSondeR"))
+
+      seasonder_apm_obj <- seasonder_readSeaSondeRAPMFile(here::here("tests/testthat/data/MeasPattern.txt"))
+
+      C <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
+
+      eigen_analysis <- seasonder_MUSICCovDecomposition(C)
+
+      test <- seasonder_MUSICEuclideanDistance(eigen_analysis, seasonder_apm_obj)
 
       expect_snapshot_value(test,style = "serialize")
 
