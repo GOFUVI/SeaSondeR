ea2e096b4c9f8ea5cc4dee6f6e2ebf34b35f17ab
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-03-09 07:20:58 +0100

    SMA-5 compute all doa solutions (#27)
    * SMA-6 (#24)
    
    Eigenvalue decomposition of MUSIC matrix
    
    Implemented:
    
    - seasonder_MUSICCovDecomposition
    
    * SMA-7 (#25)
    
    Compute euclidean distance to signal subspaces
    
    Implemented:
    
    - seasonder_MUSICEuclideanDistance
    
    * SMA-8 (#26)
    
    Extract peaks from DOAs
    
    Implemented:
    
    - seasonder_MUSICExtractPeaks

--- tests/testthat/test-SeaSondeRCS_MUSIC.R
+++ tests/testthat/test-SeaSondeRCS_MUSIC.R
@@ -3,6 +3,8 @@
 test_that("SeaSondeRCS Related functions are defined",{
 
   expect_true(is.function(seasonder_getMUSICCov))
+  expect_true(is.function(seasonder_MUSICCovDecomposition))
+  expect_true(is.function(seasonder_MUSICEuclideanDistance))
 
 
 
@@ -20,11 +22,71 @@ describe("MUSIC", {
 
       test <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
 
-      expect_snapshot_value(test,style = "deparse")
+      expect_snapshot_value(test,style = "serialize")
 
     })
 
   })
 
+  describe("seasonder_MUSICCovDecomposition",{
+
+    it("should return the eigen decomposition of the cov matrix",{
+
+      seasonder_cs_obj <- seasonder_createSeaSondeRCS(here::here("tests/testthat/data/CSS_V6.cs"), system.file("specs","CS_V1.yaml",package = "SeaSondeR"))
+
+      C <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
+
+      test <- seasonder_MUSICCovDecomposition(C)
+
+      expect_snapshot_value(test,style = "serialize")
+
+    })
+
+  })
+
+
+  describe("seasonder_MUSICEuclideanDistance",{
+
+    it("should return the euclidean distances",{
+
+      seasonder_cs_obj <- seasonder_createSeaSondeRCS(here::here("tests/testthat/data/CSS_V6.cs"), system.file("specs","CS_V1.yaml",package = "SeaSondeR"))
+
+      seasonder_apm_obj <- seasonder_readSeaSondeRAPMFile(here::here("tests/testthat/data/MeasPattern.txt"))
+
+      C <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
+
+      eigen_analysis <- seasonder_MUSICCovDecomposition(C)
+
+      test <- seasonder_MUSICEuclideanDistance(eigen_analysis, seasonder_apm_obj)
+
+      expect_snapshot_value(test,style = "serialize")
+
+    })
+
+  })
+
+
+
+  describe("seasonder_MUSICExtractPeaks",{
+
+    it("should return the euclidean distances",{
+
+      seasonder_cs_obj <- seasonder_createSeaSondeRCS(here::here("tests/testthat/data/CSS_V6.cs"), system.file("specs","CS_V1.yaml",package = "SeaSondeR"))
+
+      seasonder_apm_obj <- seasonder_readSeaSondeRAPMFile(here::here("tests/testthat/data/MeasPattern.txt"))
+
+      C <- seasonder_getMUSICCov(seasonder_cs_obj, 2,3)
+
+      eigen_analysis <- seasonder_MUSICCovDecomposition(C)
+
+      distances <- seasonder_MUSICEuclideanDistance(eigen_analysis, seasonder_apm_obj)
+
+      test <- seasonder_MUSICExtractPeaks(distances)
+
+      expect_snapshot_value(test,style = "serialize")
+
+    })
+
+  })
 
 })
