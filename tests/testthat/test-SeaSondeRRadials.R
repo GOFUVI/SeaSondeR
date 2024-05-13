485a148fc54281a2d3cd901cad93654d4012dea0
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-05-13 07:52:16 +0000

    SEAS-89
    Implemented amplitude and phase correction in SeaSondeRAPM class.
    
    Implemented:
    
    - SeaSondeRAPM_amplitude_and_phase_corrections_step_ntext.
    - SeaSondeRAPM_phase_correction_overrride_step_text
    - SeaSondeRAPM_amplitude_factors_override_step_text
    - seasonder_applyAPMAmplitudeAndPhaseCorrections
    - seasonder_readPhaseFile
    
    Updated:
    
    - seasonder_readSeaSondeRAPMFile: new override_phase_corrections, and override_amplitude_factors parameters.

--- tests/testthat/test-SeaSondeRRadials.R
+++ tests/testthat/test-SeaSondeRRadials.R
@@ -9,39 +9,19 @@ describe("short-time radials",{
   describe("compute and return a SeaSondeRShortTimeRadials object",{
 
 test_that("test 1 works with ideals",{
-  seasonder_apm_obj <- seasonder_readSeaSondeRAPMFile(here::here("tests/testthat/data/TORA/IdealPattern.txt"), override_antenna_bearing = 13.0)
+
+
+  phase_path <- here::here("tests/testthat/data/TORA/Phases.txt")
+
+  amplitude_corrections <- c(1.22398329,1.32768297)
+
+  seasonder_apm_obj <- seasonder_readSeaSondeRAPMFile(here::here("tests/testthat/data/TORA/IdealPattern.txt"), override_antenna_bearing = 13.0, override_phase_corrections = phase_path, override_amplitude_factors = amplitude_corrections, apply_phase_and_amplitude_corrections = TRUE)
+
+
+seasonder_apm_obj %<>% seasonder_applyAPMAmplitudeAndPhaseCorrections()
 
 
 
-
-
-phasec1 <-    readLines("tests/testthat/data/TORA/Phases.txt") %>% stringr::str_extract("([-\\d\\.]+)\\s*([-\\d\\.]+)",group = 1) %>% as.numeric()
-phasec2 <-    readLines("tests/testthat/data/TORA/Phases.txt") %>% stringr::str_extract("([-\\d\\.]+)\\s*([-\\d\\.]+)",group = 2) %>% as.numeric()
-
-amp1 <-  1.22398329
-
-amp2 <-  1.32768297
-
-# amp1 <-    seasonder_apm_obj %>% seasonder_getSeaSondeRAPM_AmplitudeFactors() %>% magrittr::extract(1)
-#
-# amp2 <-   seasonder_apm_obj %>% seasonder_getSeaSondeRAPM_AmplitudeFactors() %>% magrittr::extract(2)
-
-
-# amp1 <- seasonder_apm_obj %>% seasonder_getSeaSondeRAPM_AmplitudeFactors() %>% magrittr::extract(1)
-#
-# amp2 <- seasonder_apm_obj %>% seasonder_getSeaSondeRAPM_AmplitudeFactors() %>% magrittr::extract(2)
-
-   seasonder_apm_obj[1,] <- seasonder_apm_obj[1,]*amp1*exp(1i*phasec1*pi/180)
-
-   seasonder_apm_obj[2,] <- seasonder_apm_obj[2,]*amp2*exp(1i*phasec2*pi/180)
-
-   #     smoothing <- 20
-   #
-   #     seasonder_apm_obj[1,] <- complex(real = slider::slide_mean(pracma::Real(seasonder_apm_obj[1,]),before = smoothing, na_rm = T), imaginary =
-   # slider::slide_mean(pracma::Imag(seasonder_apm_obj[1,]),before = smoothing, na_rm = T))
-   #
-   #     seasonder_apm_obj[2,] <- complex(real = slider::slide_mean(pracma::Real(seasonder_apm_obj[2,]),before = smoothing, na_rm = T), imaginary =
-   #                                        slider::slide_mean(pracma::Imag(seasonder_apm_obj[2,]),before = smoothing, na_rm = T))
 
 
    plot(attr(seasonder_apm_obj, "BEAR",exact = T),Arg(seasonder_apm_obj[1,])*180/pi,xlim = c(-180,180),ylim = c(-180, 180))
