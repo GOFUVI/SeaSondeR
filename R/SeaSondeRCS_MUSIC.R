584a2b1cc7074b0c94e4cfa0c1f636df0953b5ce
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-09-30 09:45:21 +0200

    Seas 108 implement method for data.frame (#66)
    SEAS-108
    
    Implement seasonder_computeSignalSNR method for data.frames

--- R/SeaSondeRCS_MUSIC.R
+++ R/SeaSondeRCS_MUSIC.R
@@ -1399,3 +1399,47 @@ data.table::fwrite(table,file = filepath)
 
 
 }
+
+# Start SEAS-104
+
+#' @export
+seasonder_computeSignalSNR <- function(object, ...){
+  UseMethod("seasonder_computeSignalSNR")
+}
+
+
+# Start SEAS-108
+
+#' @export
+seasonder_computeSignalSNR.data.frame <- function(object, SNR, receiver_gain_dB){
+
+  # receiver_gain must be in dB
+
+  range_cell <- noise_level <- signal_power <- rlang::zap()
+
+  # Assume SNR has a range_cell column and a noise_level column in dB
+  SNR %<>% dplyr::select(range_cell, noise_level)
+
+  out <- object
+
+  # transform noise level to self spectra
+  SNR %<>% dplyr::mutate(noise_level = dB_to_self_spectra(dB_values = noise_level, receiver_gain = receiver_gain_dB))
+
+
+out %<>% dplyr::left_join(SNR, by = "range_cell")
+
+
+
+  # TODO: compute noise level for each signal using signal_power
+# Assume each row in object is a signal with an signal_power column (in self spectra units) and a range_cell column
+
+out %<>% dplyr::mutate(SNR = signal_power / noise_level)
+
+
+
+  return(out)
+}
+
+# End SEAS-108
+
+# End SEAS-104
