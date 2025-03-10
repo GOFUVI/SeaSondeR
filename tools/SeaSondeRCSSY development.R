7de96b58982893bb6faf4e071984a5a738948757
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2025-03-10 06:26:12 +0000

    seasonder_CSSY2CSData

--- tools/SeaSondeRCSSY development.R
+++ tools/SeaSondeRCSSY development.R
@@ -7,36 +7,36 @@ library(magrittr)
   specs_path <- here::here("inst/specs/CSSY_V1.yaml")
   endian <-  "big"
 
-  key_specs <-  seasonder_readYAMLSpecs(seasonder_defaultSpecsFilePath("CSSY"),c("key_size_block"))
-
-
-
-
-specs <-   seasonder_readYAMLSpecs(seasonder_defaultSpecsFilePath("CSSY"),c("CSSY","HEAD"))
-
-
-con <-   file(filepath, "rb")
-seek(con,16,origin = "start")
-header <- seasonder_readCSSYHeader(con, specs, specs_key_size = key_specs)
-dbRef <- header$dbrf$dBmReference
-
-
-seek(con,0,origin = "start")
-
-
-con <-   file(filepath, "rb")
-seek(con,8,origin = "start")
-
-key <- seasonder_readSeaSondeCSFileBlock(key_specs,con, endian)
-
-seek(con,key$size, origin = "current")
-key <- seasonder_readSeaSondeCSFileBlock(key_specs,con, endian)
-specs <-   seasonder_readYAMLSpecs(seasonder_defaultSpecsFilePath("CSSY"),c("CSSY","BODY"))
-
-
-body <- seasonder_readCSSYBody(connection = con, size= key$size, dbRef = dbRef, specs = specs,specs_key_size = key_specs)
-
-
-close(con)
-# seasonder_readSeaSondeRCSSYFile(filepath)
+#   key_specs <-  seasonder_readYAMLSpecs(seasonder_defaultSpecsFilePath("CSSY"),c("key_size_block"))
+#
+#
+#
+#
+# specs <-   seasonder_readYAMLSpecs(seasonder_defaultSpecsFilePath("CSSY"),c("CSSY","HEAD"))
+#
+#
+# con <-   file(filepath, "rb")
+# seek(con,16,origin = "start")
+# header <- seasonder_readCSSYHeader(con, specs, specs_key_size = key_specs)
+# dbRef <- header$dbrf$dBmReference
+#
+#
+# seek(con,0,origin = "start")
+#
+#
+# con <-   file(filepath, "rb")
+# seek(con,8,origin = "start")
+#
+# key <- seasonder_readSeaSondeCSFileBlock(key_specs,con, endian)
+#
+# seek(con,key$size, origin = "current")
+# key <- seasonder_readSeaSondeCSFileBlock(key_specs,con, endian)
+# specs <-   seasonder_readYAMLSpecs(seasonder_defaultSpecsFilePath("CSSY"),c("CSSY","BODY"))
+#
+#
+# body <- seasonder_readCSSYBody(connection = con, size= key$size, dbRef = dbRef, specs = specs,specs_key_size = key_specs)
+#
+#
+# close(con)
+seasonder_readSeaSondeRCSSYFile(filepath)
 
