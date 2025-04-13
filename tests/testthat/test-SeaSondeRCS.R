7bc980c421fa3993c60bc5ed4266047085a89d31
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2025-04-13 10:41:20 +0000

    test: add unit tests for SeaSondeR functions and enhance documentation with examples

---
+++ tests/testthat/test-SeaSondeRCS.R
@@ -0,0 +1,53 @@
+library(testthat)
+library(SeaSondeR)
+library(magrittr)
+
+test_that("El fichero exportado coincide con el fichero de referencia", {
+  # Definir rutas y cargar objetos
+  CS_file_path <- system.file("css_data/CSS_TORA_24_04_04_0700.cs", package = "SeaSondeR")
+  APM_path     <- system.file("css_data/MeasPattern.txt", package = "SeaSondeR")
+  
+  seasonder_apm_obj <- seasonder_readSeaSondeRAPMFile(APM_path)
+  
+  seasonder_cs_obj <- seasonder_createSeaSondeRCS(CS_file_path, seasonder_apm_object = seasonder_apm_obj)
+  
+  # Configuración del primer orden
+  first_order_settings <- list(
+    nsm = 2,
+    fdown = 10^(10 / 10),
+    flim = 10^(20 / 10),
+    noisefact = 10^(6 / 10),
+    currmax = 2,
+    reject_distant_bragg = TRUE,
+    reject_noise_ionospheric = FALSE,
+    reject_noise_ionospheric_threshold = 0
+  )
+  seasonder_cs_obj <- seasonder_computeFORs(seasonder_cs_obj, FOR_control = first_order_settings)
+  
+  # Configuración y ejecución del algoritmo MUSIC
+  MUSIC_options <- list(
+    doppler_interpolation = 2,
+    smoothNoiseLevel = TRUE,
+    PPMIN = 5, 
+    PWMAX = 50
+  )
+  seasonder_cs_obj <- seasonder_setMUSICOptions(seasonder_cs_obj, MUSIC_options)
+  seasonder_cs_obj <- seasonder_runMUSICInFOR(seasonder_cs_obj)
+  
+  # Definir segmentos angulares y exportar métricas radiales
+  AngSeg <- purrr::list_c(lapply(45:61, function(i) list(c(i, 313, 360), c(i, 0, 31))))
+  radial_metrics <- seasonder_exportLLUVRadialMetrics(
+    seasonder_cs_obj, 
+    LLUV_path = "tools/CSS_TORA_24_04_04_0700.ruv", 
+    AngSeg = AngSeg
+  )
+  
+  # Leer fichero de referencia instalado en el paquete
+  expected_metrics <- data.table::fread(
+    system.file("css_data/CSS_TORA_24_04_04_0700.ruv", package = "SeaSondeR"),
+    header = TRUE
+  )
+  
+  # Comparar los datos exportados con el fichero de referencia
+  expect_equal(radial_metrics, expected_metrics, tolerance = 1e-6)
+})
